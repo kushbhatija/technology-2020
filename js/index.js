bb63e13d04b2e6dbe83a1068cfa77fa061e556bd
kushbhatija <52459993+kushbhatija@users.noreply.github.com>
2020-03-21 18:13:29 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,37 @@
+let $techmenu= document.querySelector(".tech-menu")
+let $progressbar= document.querySelector(".progress-bar")
+
+let doc= document.documentElement
+let winH= window.pageYOffset
+let docH= doc.scrollHeight
+
+
+window.addEventListener('scroll',event=>{
+
+
+if((window.pageYOffset) >=595){
+  $techmenu.classList.add('sticky')
+}
+else{
+  $techmenu.classList.remove('sticky')
+}
+
+
+if((window.pageYOffset)>=590){
+  console.log(`scrollbar appears`)
+  $progressbar.style.display='block'
+}
+else{
+  console.log(`scrollbar disappears`)
+  $progressbar.style.display='none'
+}
+let canbescrolled= docH-winH
+
+let hasbeenscrolled=window.scrollY
+
+let percentScrolled=hasbeenscrolled/canbescrolled
+
+$progressbar.style.width= `${percentScrolled * 100}%`
+});
+
+
