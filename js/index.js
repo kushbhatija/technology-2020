c92d7962f608efeb6d282b32ad030a39c25d7127
Ahmed Husam <52460290+Ahmed641@users.noreply.github.com>
2020-04-01 14:35:41 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -115,3 +115,33 @@ savebutton.addEventListener('click',event=>{
   $savedbox.classList.toggle("saved-box-hide")
 })
 
+let loadContentFromHtmlFile = (event) => {
+
+	// Put up the loading screen
+	document.querySelector('.loading').classList.add('show')
+
+	fetch('loadthis.html')
+		.then((response) => {
+			return response.text() // Convert it something readable
+		})
+		.then((html) => {
+
+			let parser = new DOMParser()
+			let newDocument = parser.parseFromString(html, 'text/html')
+
+			// The loadthis.html page has been loaded!
+			// Append the content from the new page into the old page
+			document.querySelector('#content').innerHTML += newDocument.querySelector('#content').innerHTML
+			
+			// Take down the loading screen, we're done
+			document.querySelector('.loading').classList.remove('show')
+		})
+
+		
+
+		
+
+}
+
+document.querySelector('#load').addEventListener('click', loadContentFromHtmlFile)
+
